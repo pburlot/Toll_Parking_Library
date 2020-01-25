f67fee7b6000a2dc9a19cdce28b22943970fbaed
philippe <phil2813@hotmail.fr>
2020-01-25 15:06:19 +0100

    Init spring boot microservice + maven

---
+++ src/test/java/com/microservice/tollparking/TollparkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.tollparking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollparkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
